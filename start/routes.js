233ff646db3f266b227698b39c25da31abf22694
0zz1cl13 <seniorcl13@gmail.com>
2020-01-07 17:02:35 -0400

    feat(property):crud das propriedades

--- start/routes.js
+++ start/routes.js
@@ -18,3 +18,9 @@ const Route = use('Route')
 
 Route.post('/users', 'UserController.create');
 Route.post('/sessions', 'SessionController.create');
+
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
+
+// Route.get('/properties/:id', 'PropertyController.update')
